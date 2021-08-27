5a22b93fcf657575c17920923becf2d17bee83aa
Alan <aln447@gmail.com>
2021-08-27 18:17:55 +0200

    Initial commit

---
+++ db/mongoose.js
@@ -0,0 +1,142 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+// todo: put me in an env!!
+mongoose.connect('mongodb://127.0.0.1:27017/written-by', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  // we're connected!
+})
+
+
+// define the country model
+const countrySchema = new mongoose.Schema({
+    name: {
+      type: String,
+      unique: true,
+    },
+    languange: String,
+});
+
+const Country = mongoose.model('Country', countrySchema)
+
+
+// define the genre mondel
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+  }
+});
+const Genre = mongoose.model('Genre', genreSchema);
+
+// define the storyType model
+const storyTypeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+  }
+});
+const StoryType = mongoose.model('StoryType', storyTypeSchema);
+
+// define the story model
+const storySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    max: 255,
+    required: true,
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  length: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  createdAt: Date,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  language: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Country',
+    required: true,
+  },
+  type: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'StoryType',
+    required: true,
+  },
+  genre: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Genre',
+    required: true,
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  },
+  outClicks: {
+    type: Number,
+    default: 0,
+  },
+}, { timestamps: true });
+
+const Story = mongoose.model('Story', storySchema)
+
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+  birthday: {
+    type: Date,
+   
+  },
+  bio: {
+    type: String,
+    required: false,
+  },
+  country: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Country',
+  },
+  stories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Story',
+  }]
+})
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+      user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+})
+
+const User = mongoose.model('User', userSchema)
+// define the user model
+module.exports = { Country, Genre, StoryType, Story, User };
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const { Country, Genre, StoryType, Story, User } = require('./db/mongoose.js')
+
+const app = express()
+const port = 8000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Just the default page, keep lookin\'')
+})
+
+app.post('/add-country', (req, res) => {
+    const newCountry = new Country(req.body)
+
+    newCountry.save((err, country) => {
+        err ? res.status(500).send(err) : res.send(JSON.stringify(country))
+    })
+})
+
+app.get('/list-countries', async (req, res) => {
+    const countries = await Country.find().exec();
+
+    res.json(countries);
+});
+
+app.post('/add-genre', (req, res) => {
+    const newGenre = new Genre(req.body)
+
+    newGenre.save((err, genre) => {
+        err ? res.status(500).send(err) : res.send(JSON.stringify(genre))
+    })
+})
+
+app.get('/list-genres', async (req, res) => {
+    const genres = await Genre.find().exec();
+
+    res.json(genres);
+});
+
+app.post('/add-story-type', (req, res) => {
+    const newType = new StoryType(req.body)
+
+    newType.save((err, type) => {
+        err ? res.status(500).send(err) : res.send(JSON.stringify(type))
+    })
+})
+
+app.get('/list-story-types', async (req, res) => {
+    const types = await StoryType.find().exec();
+
+    res.json(types);
+});
+
+app.post('/add-user', (req, res) => {
+    const newUser = new User(req.body)
+
+    newUser.save((err, user) => {
+        err ? res.status(500).send(JSON.stringify(err)) : res.send(JSON.stringify(user))
+    })
+})
+
+app.get('/list-users', async (req, res) => {
+    const users = await User.find().populate('country').populate('stories', 'username birthday').exec();
+
+    res.json(users);
+});
+
+app.post('/add-story', (req, res) => {
+    const newStory = new Story(req.body)
+
+    newStory.save((err, story) => {
+        err ? res.status(500).send(err) : res.send(JSON.stringify(story))
+    })
+})
+
+app.get('/list-stories', async (req, res) => {
+    const stories = await Story.find().populate('author').populate('genre').populate('type').exec();
+
+    res.json(stories);
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+}) 
