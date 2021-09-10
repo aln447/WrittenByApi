cbe6faa955ccdd95a6c9c3387573a5775f05a96e
Alan <aln447@gmail.com>
2021-09-10 21:29:45 +0200

    Relocate the address into the env

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// todo: put me in an env!!
-mongoose.connect('mongodb://127.0.0.1:27017/written-by', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.ADDRESS, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'));
